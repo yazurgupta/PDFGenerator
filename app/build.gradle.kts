49a612ea5b5ce9947f0d23863f6e89c9a0fcfb7d
yazurgupta <yazurgupta145@gmail.com>
2026-07-11 03:49:22 +0530

    Updated versionName to 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.biswariapps.pdfgenerator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3"
+        versionCode = 6
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
